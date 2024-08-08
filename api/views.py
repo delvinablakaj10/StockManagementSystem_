48e4af6ced3db1354d44e316eaebfcde48610791
delvinablakaj10 <d.blakaj1108@gmail.com>
2024-08-08 14:01:34 +0200

    Added views, urls

--- api/views.py
+++ api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    title = "This is homepage"
+    context = {"title": title}
+    return render(request, "home.html", context)
+
