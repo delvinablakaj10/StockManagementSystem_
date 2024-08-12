e204f1896ca1a1373641d76c1f89779fb0f6187b
delvinablakaj10 <d.blakaj1108@gmail.com>
2024-08-12 13:45:34 +0200

    Export to csv table of items

--- api/views.py
+++ api/views.py
@@ -1,3 +1,6 @@
+import csv
+
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
 from .forms import StockCreateForm, StockSearchForm, StockUpdateForm
@@ -14,13 +17,21 @@ def list_items(request):
     header = "List of items"
     form = StockSearchForm(request.POST or None)
     queryset = Stock.objects.all()
-    context = {"header": header, "queryset": queryset, "form": form}
     if request.method == "POST":
         queryset = Stock.objects.filter(
             category__name__icontains=form["category"].value(),
             item_name__icontains=form["item_name"].value(),
         )
-        context = {"form": form, "header": header, "queryset": queryset}
+    if form["export_to_CSV"].value() == True:
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = 'attachment; filename="List of stock.csv"'
+        writer = csv.writer(response)
+        writer.writerow(["CATEGORY", "ITEM NAME", "QUANTITY"])
+        instance = queryset
+        for stock in instance:
+            writer.writerow([stock.category, stock.item_name, stock.quantity])
+        return response
+    context = {"form": form, "header": header, "queryset": queryset}
 
     return render(request, "list_items.html", context)
 
@@ -42,7 +53,7 @@ def update_items(request, pk):
         form = StockUpdateForm(request.POST, instance=queryset)
         if form.is_valid():
             form.save()
-            return redirect('/list_items')
+            return redirect("/list_items")
         context = {"form": form}
 
     return render(request, "add_items.html", context)
@@ -52,5 +63,5 @@ def delete_items(request, pk):
     queryset = Stock.objects.get(id=pk)
     if request.method == "POST":
         queryset.delete()
-        return redirect('/list_items')
-    return render(request, 'delete_items.html')
+        return redirect("/list_items")
+    return render(request, "delete_items.html")
